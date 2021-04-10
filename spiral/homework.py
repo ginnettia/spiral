4b1f288f56f854c49f9dafbd00f6e3b224a21ef7
ginnettia <adamhginnetti@gmail.com>
2021-04-09 22:26:27 -0400

    testing black 3

--- spiral/homework.py
+++ spiral/homework.py
@@ -2,7 +2,7 @@ def spiralize(number):
     #Declare n
     n = (number - 1) /2
     #Declare x
-    x = (3 + 2 * n * (8 * n * n + 15 * n + 13)) /3
+    x = (3 + 2 * n * (8 * n * n + 15 * n + 13))/3
     return_value = x
     #return the value
     return return_value
