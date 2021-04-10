6ae111e670e8e5e5c98c20d11bd3339bc759a5c7
ginnettia <adamhginnetti@gmail.com>
2021-04-09 20:13:37 -0400

    making changes

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,4 +1,8 @@
 def spiralize(number):
-    return_value = 1
-
+    #Declare n
+    n = (number - 1) /2
+    #Declare x
+    x = (3 + 2 * n * (8 * n * n + 15 * n + 13)) /3
+    return_value = x
+    #return value
     return return_value
